c0241e6a48ed7dad84ac642a0d34947d61beadae
Will <48894810+Twillij@users.noreply.github.com>
2020-09-02 16:57:02 +1000

    WIP animating device

--- SliceOfWife/Source/SliceOfWife/AnimatingDevice.h
+++ SliceOfWife/Source/SliceOfWife/AnimatingDevice.h
@@ -15,6 +15,8 @@ public:
 	// Sets default values for this actor's properties
 	AAnimatingDevice();
 
+	class AAssemblyTable* assemblyTable = nullptr;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
